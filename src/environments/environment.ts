969cec0d8b4d67c81357ee944e4ff082757be38e
millooo94 <camillomusmeciunict@gmail.com>
2025-04-04 23:03:31 +0200

    add firebase settings

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBYEsdGSu24Bju-iqghrQkqApHhQVpgY-M",
+    authDomain: "ssg-blog-test.firebaseapp.com",
+    projectId: "ssg-blog-test",
+    storageBucket: "ssg-blog-test.firebasestorage.app",
+    messagingSenderId: "932442747760",
+    appId: "1:932442747760:web:d829bb35f33deabdbfd5b5",
+    measurementId: "G-KWDECT6LQ5",
+  },
+};
